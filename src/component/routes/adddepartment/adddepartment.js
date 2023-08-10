6cf844c4d4db3bc73ce519c25ad0edbb1300066c
abbassu <abbassurakji99@gmail.com>
2023-08-10 16:56:58 +0300

    first commiddt

--- src/component/routes/adddepartment/adddepartment.js
+++ src/component/routes/adddepartment/adddepartment.js
@@ -2,77 +2,138 @@ import React from "react";
 import ImageSlider from "../../imageslider/ImageSlider";
 import "./adddepr.scss";
 function Adddepartment() {
+  let b = 140;
+  let f = 20;
+  let offer = 1;
   return (
     <div className="testdepart">
-      {/* <ImageSlider /> */}
-
-      {/* <div className="pproductcard">
-        <ImageSlider />
-      </div> */}
-
       <div className="pproductcard">
         <ImageSlider />
         <div className="addtioninformation">
-          <strong>DELL Bag kdkddkkkkkkkkkkkkklllllllll</strong>
-          <br />
-
-          <span>
-            <div className="percentCost"></div>
-          </span>
+          <div dir="rtl">
+            <strong>لابتوب ديل XPS</strong>
+            <br />
+            {offer ? (
+              <>
+                <div>
+                  <div className="afteroffer">
+                    {" "}
+                    <strong className="">{b - b * (20 / 100)} </strong>
+                    <i class="fa-solid fa-shekel-sign"></i>
+                  </div>
+                  <div className="">
+                    <span className="beforeoffer"> {b}</span>
+                    <span className="inoffer">خصم %{f} </span>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="afteroffer" dir="rtl">
+                  <strong>{b} </strong>
+                  <i class="fa-solid fa-shekel-sign"></i>
+                </div>
+              </>
+            )}
+          </div>
         </div>
       </div>
 
       <div className="pproductcard">
         <ImageSlider />
         <div className="addtioninformation">
-          <strong>DELL Bag kdkddkkkkkkkkkkkkklllllllll</strong>
-          <br />
-
-          <span>
-            <div className="percentCost"></div>
-          </span>
+          <div dir="rtl">
+            <strong>لابتوب ديل XPS</strong>
+            <br />
+            {offer ? (
+              <>
+                <div>
+                  <div className="afteroffer">
+                    {" "}
+                    <strong className="">{b - b * (20 / 100)} </strong>
+                    <i class="fa-solid fa-shekel-sign"></i>
+                  </div>
+                  <div className="">
+                    <span className="beforeoffer"> {b}</span>
+                    <span className="inoffer">خصم %{f} </span>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="afteroffer" dir="rtl">
+                  <strong>{b} </strong>
+                  <i class="fa-solid fa-shekel-sign"></i>
+                </div>
+              </>
+            )}
+          </div>
         </div>
       </div>
 
       <div className="pproductcard">
         <ImageSlider />
         <div className="addtioninformation">
-          <strong>DELL Bag kdkddkkkkkkkkkkkkklllllllll</strong>
-          <br />
-
-          <span>
-            <div className="percentCost"></div>
-          </span>
+          <div dir="rtl">
+            <strong>لابتوب ديل XPS</strong>
+            <br />
+            {offer ? (
+              <>
+                <div>
+                  <div className="afteroffer">
+                    {" "}
+                    <strong className="">{b - b * (20 / 100)} </strong>
+                    <i class="fa-solid fa-shekel-sign"></i>
+                  </div>
+                  <div className="">
+                    <span className="beforeoffer"> {b}</span>
+                    <span className="inoffer">خصم %{f} </span>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="afteroffer" dir="rtl">
+                  <strong>{b} </strong>
+                  <i class="fa-solid fa-shekel-sign"></i>
+                </div>
+              </>
+            )}
+          </div>
         </div>
       </div>
 
       <div className="pproductcard">
         <ImageSlider />
         <div className="addtioninformation">
-          <strong>DELL Bag kdkddkkkkkkkkkkkkklllllllll</strong>
-          <br />
-
-          <span>
-            <div className="percentCost"></div>
-          </span>
+          <div dir="rtl">
+            <strong>لابتوب ديل XPS</strong>
+            <br />
+            {offer ? (
+              <>
+                <div>
+                  <div className="afteroffer">
+                    {" "}
+                    <strong className="">{b - b * (20 / 100)} </strong>
+                    <i class="fa-solid fa-shekel-sign"></i>
+                  </div>
+                  <div className="">
+                    <span className="beforeoffer"> {b}</span>
+                    <span className="inoffer">خصم %{f} </span>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="afteroffer" dir="rtl">
+                  <strong>{b} </strong>
+                  <i class="fa-solid fa-shekel-sign"></i>
+                </div>
+              </>
+            )}
+          </div>
         </div>
       </div>
-
-      {/* <div className="pproductcard">
-        <ImageSlider />
-      </div>
-
-      <div className="pproductcard">
-        <ImageSlider />
-      </div>
-
-      <div className="pproductcard">
-        <ImageSlider />
-      </div>
-
-      <div className="pproductcard">
-        <ImageSlider />
-      </div> */}
     </div>
   );
 }
